d46accfd7d9a9dbdd145b5730fd338717b89f380
SnehalPatra-dev <77398743+SnehalPatra-dev@users.noreply.github.com>
2021-03-30 17:23:23 +0530

    Add files via upload
    DOM element test cases

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -2,7 +2,13 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
 describe('AppComponent', () => {
+
+  let fixture, app;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -14,22 +20,125 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+
+  beforeEach(async () => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+  });
+
+  // Test 0
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    expect(app).toBeDefined();
+  });
+
+  // Test 1
+  it('should create the app', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project2todo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('project2todo');
+  // Test 2
+  it(`should have as title 'Tasks at hand:'`, () => {
+    expect(app.title).toEqual('Tasks at hand:');
   });
 
+  // Test 3
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('project2todo app is running!');
+    expect(compiled.querySelector('#headingid1').textContent).toContain('Tasks at hand:');
   });
+
+  // Test 4
+  it('text content should not be null', () => {
+    //Arrange
+    fixture.detectChanges();
+    const e1 = fixture.debugElement.query(By.css('.heading1')).nativeElement;
+
+    //Assert
+    expect(e1.innerHTML).not.toBeNull();
+    expect(e1.innerHTML.length).toBeGreaterThan(0);
+
+  });
+
+  // Test 5
+  it('content of icon should not be null', () => {
+    // Arrange
+    fixture.detectChanges();
+    const e1 = fixture.debugElement.query(By.css('#icon1')).nativeElement;
+
+    //Assert
+    expect(e1.innerHTML).not.toBeNull();
+  });
+
+  //Test 6
+  it('icon1 should be defined', () => {
+    // Arrange
+    fixture.detectChanges();
+    const e1 = fixture.debugElement.query(By.css('#icon1')).nativeElement;
+
+    //Assert
+    expect(e1.innerHTML).toBeDefined();
+  });
+
+  //Test 7 
+  it('icon2 should be defined', () => {
+    // Arrange 
+    fixture.detectChanges();
+    const e2 = fixture.debugElement.query(By.css('#icon2')).nativeElement;
+
+    //Assert
+    expect(e2.innerHTML).toBeDefined();
+  });
+
+  //Test 8 
+  it('icon1 should not be null', () => {
+    // Arrange 
+    fixture.detectChanges();
+    const e1 = fixture.debugElement.query(By.css('#icon1')).nativeElement;
+
+    //Assert
+    expect(e1.innerHTML).not.toBeNull();
+  });
+
+  //Test 9
+  it('icon2 should not be null', () => {
+    // Arrange 
+    fixture.detectChanges();
+    const e2 = fixture.debugElement.query(By.css('#icon2')).nativeElement;
+
+    //Assert 
+    expect(e2.innerHTML).not.toBeNull();
+  });
+
+  //Test 10
+  it('icon2 should contain the specified text', () => {
+    // Arrange 
+    fixture.detectChanges();
+    const e2 = fixture.debugElement.query(By.css('#icon2')).nativeElement;
+
+    // Assert
+    expect(e2.textContent).toContain('add_task');
+  });
+
+  //Test 11
+  it('icon1 should contain the specified text', () => {
+    // Arrange 
+    fixture.detectChanges();
+    const e1 = fixture.debugElement.query(By.css('#icon1')).nativeElement;
+
+    // Assert
+    expect(e1.textContent).toContain('border_color');
+  });
+
+  //Test 12
+  it('button1 should be defined and non-empty', () => {
+    // Arrange
+    fixture.detectChanges();
+    const b1 = fixture.debugElement.query(By.css('.button1')).nativeElement;
+
+    // Assert
+    expect(b1.innerHTML).not.toBeNull();
+    expect(b1.innerHTML).toBeDefined();
+  });
+
 });
